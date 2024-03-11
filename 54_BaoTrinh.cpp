b1f3554dc30f0e5b00c4436cfbaf41e90dfece7d
BaoTrinh25 <btrinh2505@gmail.com>
2024-03-11 11:31:06 +0700

    Them nd

--- 54_BaoTrinh.cpp
+++ 54_BaoTrinh.cpp
@@ -3,11 +3,7 @@
 int main() {
 	std::cout << "Hello World!";
 	std::cout << "Thêm 1 dòng mới";
-<<<<<<< HEAD
-	std::cout << "54_MAin thay đổi";
-=======
-	std::cout << "54_BT";
-std::cout << "54_BT Merge";
+
 >>>>>>> 54_BaoTrinh
 	return 0;
 }
